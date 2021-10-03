219db9133b91b0ccf40075619af87dd6487a9500
Nicolas Mantzos <georgeos.nicolas83@gmail.com>
2021-10-02 22:50:12 -0300

    [12] file upload routes implementation

--- routes.js
+++ routes.js
@@ -4,11 +4,8 @@ const dbConfig = require('./dbconfig/dbConfig');
 const router = express.Router();
 const db = require('./dbconfig/dbConfig');
 const project = require('./services/ProjectService');
-<<<<<<< HEAD
 const multer  = require('multer')
 const upload = multer({ dest: './uploads' })
-=======
->>>>>>> 6d46aece1b0e6370ce079b3b3cb15d53b8dea349
 
 router.get('/', (req, res) =>{
     res.json({
@@ -16,14 +13,13 @@ router.get('/', (req, res) =>{
     });
 });
 
-<<<<<<< HEAD
 router.post('/upload', async (req, res) => {
     const arquivo = {filename: req.files['file']['name'], bytecontent: req.files['file']['data'], projectid: 1}
     db.query('INSERT INTO FILE(filename,bytecontent,projectid) VALUES ($1,$2,$3) RETURNING *', [arquivo.filename,arquivo.bytecontent,arquivo.projectid]);
     res.send(arquivo);
         // res.sendStatus(400);
 })
-=======
+
 router.post('/projeto/cadastro', async function(req, res) {//Falta tratamento dos dados
 
     try {
@@ -32,18 +28,8 @@ router.post('/projeto/cadastro', async function(req, res) {//Falta tratamento do
         console.error('Erro durante a criação do projeto', err.message);
     }
 
-    // var body = req.body;
-    // res.status = 200
 
-    // var result = db.query('INSERT INTO PROJETO(descricao,concluido,aprovado,agente_externo,disciplina_aloc) VALUES ($1,$2,$3,$4,$5) RETURNING *', [body.descricao,body.concluido,body.aprovado,body.agente_externo,body.disciplina_aloc]).then((response) => {
-    //     res.status(200).json({ response: response.rows });
-    //     })
-    //     .catch((response) => {
-    //         res.status(400).json({ response: response });
-    //     });
 });
-
->>>>>>> 6d46aece1b0e6370ce079b3b3cb15d53b8dea349
 
 router.get('/projeto/visualizar-arquivo/:idArquivo', (req, res) =>{
     var response = db.query('SELECT f.bytecontent, f.filename FROM FILE as f WHERE fileid=$1', [req.params.idArquivo]).then(response =>{
--- services/ProjectService.js
+++ services/ProjectService.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 const db = require('../dbconfig/dbConfig');
 
 async function create(project) {
-    console.log(project.file);
     var result = await db.query('INSERT INTO PROJECT(name,problem,expectedresult,knowledgearea,status,userid) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',
         [project.name,project.problem,project.expectedresult,project.knowledgearea,project.status, project.userid]);
     let message;
@@ -21,23 +19,4 @@ async function getProject() {
 
 module.exports = {
     create
-=======
-const db = require('../dbconfig/dbConfig');
-
-async function create(project){
-    var result = await db.query('INSERT INTO PROJETO(descricao,concluido,aprovado,agente_externo,disciplina_aloc) VALUES ($1,$2,$3,$4,$5) RETURNING *', 
-                                [project.descricao,project.concluido,project.aprovado,project.agente_externo,project.disciplina_aloc]);
-    let message= 'Erro na criacao do projeto';
-
-    if (result.affectedRows){
-        message = 'Projeto criado com sucesso'
-    }
-    
-    return {message};
-
-}
-
-module.exports = {
-    create
->>>>>>> 6d46aece1b0e6370ce079b3b3cb15d53b8dea349
 }
